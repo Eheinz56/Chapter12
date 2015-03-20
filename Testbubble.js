785b937341d475c50a3a0f4f0ae126d7e65e60e4
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-03-20 10:58:33 -0400

    testbubble file

---
+++ Testbubble.js
@@ -0,0 +1,9 @@
+
+load("arrayB.js");
+var numElements = 10;
+var mynums = new CArray(numElements);
+mynums.setData();
+print(mynums.toString());
+mynums.insertionSort();
+print();
+print(mynums.toString());
